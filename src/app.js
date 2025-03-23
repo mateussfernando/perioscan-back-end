01269fab2ffc1f586aec33c465f55a9761029a0f
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2025-03-23 03:04:07 +0000

    configurações iniciais no app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const morgan = require('morgan');
 const dotenv = require('dotenv');
 
+// Carregar variáveis de ambiente
+dotenv.config()
+
+// Inicializar Express
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req,res) => {
+    res.end("API esta funcionando!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
