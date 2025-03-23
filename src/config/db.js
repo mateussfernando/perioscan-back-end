5fd1ada5b8c3c8702e3e39cee43a75113ff69ac7
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2025-03-23 20:30:59 +0000

    tratamento de erro no db.js e remoção do .env no git ignore

--- src/config/db.js
+++ src/config/db.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,      // Usa o novo parser de URL
+      useUnifiedTopology: true,  // Usa o novo mecanismo de monitoramento
+    });
+    console.log('MongoDB conectado!');
+  } catch (err) {
+    console.error('Erro ao conectar ao MongoDB:', err.message);
+    process.exit(1);  // Encerra o processo em caso de erro
+  }
+};
