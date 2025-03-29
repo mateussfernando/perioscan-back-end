8fc0f19422364a7dca474d09a78105b6ca0744fb
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2025-03-29 00:34:19 +0000

    iniciando as configuraçoes do app.js na nova branch

--- src/app.js
+++ src/app.js
@@ -3,5 +3,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
-
 const app = express();
+const port = 3337;
+
+app.get("/", (req, res) => {
+    res.end("O servidor esta rodando")
+} )
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+  });
+
+
+
