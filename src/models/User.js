b510c7978934f322a30a24f257292860b1134f09
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2025-03-29 01:24:36 +0000

    modelo do usuario feito e arquivo de configuração do banco de dados feito

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'perito', 'assistente'],
+    default: 'assistente',
+  },
+});
+
+// Hash da senha antes de salvar
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+module.exports = mongoose.model('User', UserSchema);
