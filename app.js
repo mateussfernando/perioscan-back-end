730c6e293c97f5df4da59fa3cea7ddbc047671a1
Gabriel Di Lucas <165342425+dilucas00@users.noreply.github.com>
2025-04-03 14:10:02 +0000

    alteraçao no codigo do app, para comtemplar o cors

--- app.js
+++ app.js
@@ -28,7 +28,16 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // Middlewares
-app.use(cors())
+app.use(cors({
+  origin: [
+    'https://glowing-enigma-97644xjvgq65h75jw-3000.app.github.dev',
+    'http://localhost:3000'
+    // Adicione outras origens conforme necessário
+  ],
+  credentials: true, // Importante para permitir cookies/credenciais
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
 app.use(helmet())
