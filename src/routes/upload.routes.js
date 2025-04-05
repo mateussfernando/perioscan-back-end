36239f6089746327ec2120d4bb9f3cfcb94b277e
Gabriel Di Lucas <165342425+dilucas00@users.noreply.github.com>
2025-04-05 03:42:55 +0000

    alterações no sistema de upload

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,19 +1,15 @@
+import express from "express"
+import { uploadImage } from "../controllers/upload.controller.js"
+import { protect } from "../middleware/auth.middleware.js"
+import upload from "../middleware/upload.middleware.js"
 
-
-import express from "express";
-import { uploadImage, deleteCloudinaryImage } from "../controllers/upload.controller.js";
-import { protect, authorize } from "../middleware/auth.middleware.js";
-import { handleUploadErrors } from "../middleware/upload.middleware.js";
-
-const router = express.Router();
+const router = express.Router()
 
 // Proteger todas as rotas
-router.use(protect);
+router.use(protect)
 
 // Rota para upload de imagem
-router.post("/", handleUploadErrors, uploadImage);
+router.post("/", upload.single("image"), uploadImage)
 
-// Rota para excluir imagem do Cloudinary
-router.delete("/:publicId", authorize("admin", "perito"), deleteCloudinaryImage);
+export default router
 
-export default router;
